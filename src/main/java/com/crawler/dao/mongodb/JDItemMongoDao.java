8aa3dab6956a777a60cfc15494ecfeda6f216c66
yujiwei <774346810@qq.com>
2019-01-15 17:43:52 +0800

    删除单元测试

---
+++ src/main/java/com/crawler/dao/mongodb/JDItemMongoDao.java
@@ -0,0 +1,7 @@
+package com.crawler.dao.mongodb;
+
+/**
+ *
+ */
+public interface JDItemMongoDao {
+}
---
+++ src/main/java/com/crawler/dao/mongodb/impl/JDItemMongoDaoImpl.java
@@ -0,0 +1,11 @@
+package com.crawler.dao.mongodb.impl;
+
+import com.crawler.dao.mongodb.JDItemMongoDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ */
+@Repository
+public class JDItemMongoDaoImpl implements JDItemMongoDao {
+}
